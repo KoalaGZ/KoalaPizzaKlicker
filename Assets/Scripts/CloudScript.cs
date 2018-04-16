9411cb62eccd85e33b20972ef3d4e7c71cd8d1d8
OttoDan <33697887+OttoDan@users.noreply.github.com>
2018-04-16 14:07:31 +0200

    added OutdoorSprites and scene + pizza master koala

---
+++ Assets/Scripts/CloudScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudScript : MonoBehaviour {
+    float speed = 4f;
+    float rightcornerX=8;
+    float oldX;
+	// Use this for initialization
+	void Start () {
+        oldX = transform.position.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += Vector3.right * speed * Time.deltaTime;
+        if (transform.position.x > rightcornerX)
+            transform.position = new Vector3(oldX,transform.position.y, transform.position.z);
+	}
+}
