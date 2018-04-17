f6dca7712bee85e62280353316c5cd97f51343e7
jannik funke <jannik.f@online.de>
2018-04-17 09:53:44 +0200

    pull
    stapel.cs meh

--- Assets/Scripts/Stapel.cs
+++ Assets/Scripts/Stapel.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Stapel : MonoBehaviour {
+
+    public GameObject pizzaKartonPrefab;
+    int kartonOffset;
+
     public enum Type
     {
         PizzaKarton,
@@ -11,15 +15,29 @@ public class Stapel : MonoBehaviour {
         Flour
     }
     public Type type;
+
     public int amount = 0;
 
     public void AddElement()
     {
+        Instantiate(pizzaKartonPrefab, transform.position + kartonOffset * Vector3.right, Quaternion.identity, transform);
+    }
 
+    public void kartonRotation()
+    {
+        
+            
     }
 
     public void RemoveElement()
     {
-
+        if (!pizzaKartonPrefab)
+        {
+            Destroy(transform.GetChild(transform.childCount).gameObject);
+        }
+    }
+    private void Update()
+    {
+        kartonOffset = transform.childCount;
     }
 }
