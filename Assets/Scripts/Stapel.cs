408e4735fccd08842e134370084f7153e7d0ecad
jannik funke <jannik.f@online.de>
2018-04-17 10:34:46 +0200

    Summary
    Description

--- Assets/Scripts/Stapel.cs
+++ Assets/Scripts/Stapel.cs
@@ -6,6 +6,9 @@ public class Stapel : MonoBehaviour {
 
     public GameObject pizzaKartonPrefab;
     int kartonOffset;
+    int frameCount;
+    float duration;
+    public float turnSpeed;
 
     public enum Type
     {
@@ -20,12 +23,17 @@ public class Stapel : MonoBehaviour {
 
     public void AddElement()
     {
-        Instantiate(pizzaKartonPrefab, transform.position + kartonOffset * Vector3.right, Quaternion.identity, transform);
+        Transform newElement =  Instantiate(pizzaKartonPrefab, transform.position + kartonOffset * new Vector3(0.1f, 0f, 0f), Quaternion.identity, transform).transform;
+        StartCoroutine(kartonRotation(newElement));
     }
 
-    public void kartonRotation()
+    IEnumerator kartonRotation(Transform tKarton)
     {
-        
+        for(float t = 0; t < duration; t ++)
+        {
+            tKarton.Rotate(Vector3.forward * turnSpeed * Time.deltaTime, t);
+            yield return new WaitForEndOfFrame();
+        }        
             
     }
 
@@ -36,8 +44,19 @@ public class Stapel : MonoBehaviour {
             Destroy(transform.GetChild(transform.childCount).gameObject);
         }
     }
+    private void Start()
+    {
+        duration = 270f;
+        turnSpeed = 1f;
+    }
     private void Update()
     {
         kartonOffset = transform.childCount;
+        frameCount++;
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            AddElement();
+        }
     }
 }
